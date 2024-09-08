2890c814b5f10fe64eb6992fd2340b9ff69b4699
Danben Erick <danbencb08@outlook.es>
2024-09-07 19:19:47 -0500

    Cambios

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,51 +1,71 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import axios from 'axios'
 
 const LoginPage = ({ logined, setLogined }) => {
   const [formData, setFormData] = useState({
-    usuario: '',
-    password: ''
-  })
+    usuario: "",
+    password: "",
+  });
 
-  const autenticado = () => {
-    setLogined(true)
-    alert(`Credenciales ${formData.usuario} ${formData.password}`)
-  }
+  const autenticado = async () => {
+    // setLogined(true);
+    alert(`Credenciales ${formData.usuario} ${formData.password}`);
+    const result_login = await axios.post('http://localhost:8004/cepre-login', { usuario: formData.usuario, password: formData.password });
+    console.log(result_login)
+  };
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setFormData({
       ...formData,
       [name]: value, // Actualiza solo el campo correspondiente en el estado
     });
-  }
+  };
 
   return (
-    <div>
-      <div className="login-container">
-        <h1 className="login-title">Iniciar Sesión</h1>
-        <form className="login-form">
-            <div className="input-group">
-                <label>Correo Electrónico</label>
-                <input
-                  type="text"
-                  name="usuario"
-                  value={formData.usuario}
-                  onChange={handleInputChange}
-                />
-            </div>
-            <div className="input-group">
-                <label>Contraseña</label>
-                <input 
-                  type="password"
-                  name="password"
-                  value={formData.password}
-                  onChange={handleInputChange}
-                />
-            </div>
-            <button type="button" onClick={autenticado} className="login-button">Ingresar</button>
-        </form>
-    </div>
-    </div>
-  )
-}
+    <>
+    <div className="background-login"></div>
+    <div className="PageLogin">
+      <div className="container-form">
 
-export default LoginPage
+        <h2 className="" style={{ fontWeight: "bold" }}>
+          <img src={`${process.env.PUBLIC_URL}/undac_admision.jpg`} alt="Admisión UNDAC" />
+          CEPRE - UNDAC
+        </h2>
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">
+            Usuario
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Usuario"
+            value={formData.usuario}
+            name="usuario"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">
+            Contraseña
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Contraseña"
+            value={formData.password}
+            name="password"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="mb-3">
+          <button className="btn btn-primary" style={{ width: "100%" }} onClick={autenticado}>
+            Continuar
+          </button>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default LoginPage;
